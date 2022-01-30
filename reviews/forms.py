d564ae661be00391680726f83da1e0ab1035050e
lordofcodes <mister.spablo@gmail.com>
2022-01-30 12:26:19 +0100

    model forms

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,10 +1,31 @@
 from django import forms
+from .models import Review
+
+# FORM WHICH USE STANDARD FORMS
+# RESULT IS THE SAME OF BOTH OF THOSE CODES
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
+#         "required": "Your name must not be empty",
+#         "max_length": "please enter shorter name"
+#     })
+#     review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
 
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
-        "required": "Your name must not be empty",
-        "max_length": "please enter shorter name"
-    })
-    review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
-    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+# OTHER PHILOSOPHY WHICH BASE ON ALREADY EXISTING FORMS ON MODELS.PY
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'  # optionally you can select ALL
+        # exclude = ['']  # or you can exclude some fields from models
+        labels = {
+            "user_name": "Your Name",
+            "review_text": "Your Feedback",
+            "rating": "Your rating"
+        }
+        error_messages = {
+            "user_name": {
+                "required": "Your name must not be empty!",
+                "max_length": "please enter shorter name"
+            }
+        }
--- reviews/views.py
+++ reviews/views.py
@@ -7,14 +7,17 @@ from .models import Review
 # Create your views here.
 def review(request):
     if request.method == 'POST':
-        form = ReviewForm(request.POST)
+        existing_data = Review.objects.get(pk=1)
+        form = ReviewForm(request.POST, instance=existing_data)  # instance allows updating existing data
 
         if form.is_valid():
-            review = Review(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating'])
-            review.save()
+            form.save()
+            # THIS IS REQUIRED IF WE USE FORMS METHOD NOT MODEL.FORMS.. THIS IS SIMPLE NOW THIS METHOD SAVE CODE
+            # # review = Review(
+            # #     # user_name=form.cleaned_data['user_name'],
+            # #     # review_text=form.cleaned_data['review_text'],
+            # #     # rating=form.cleaned_data['rating'])
+            # review.save()
             return HttpResponseRedirect("/thank-you")
     else:
         form = ReviewForm()
